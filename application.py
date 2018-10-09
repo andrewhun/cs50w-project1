555f892837bdf3f6ad8ed583687ce2c7922ea77c
andrewhun <40831919+andrewhun@users.noreply.github.com>
2018-10-09 19:53:14 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -54,111 +54,84 @@ comments = Table("comments", metadata,
 # create a session for the database
 db = scoped_session(sessionmaker(bind=engine))
 
-'''
-ORM-version (does not work)
-
-Base = declarative_base()
-# create a class which will be connected to the users table in the database
-class User(Base):
-	__tablename__ = "users"
-
-	id = Column(Integer, Sequence("users_id_seq"))
-	username = Column(String, primary_key = True)
-	hashed = Column(String)
-
-	def __repr__(self):
-		return ("<User(username='%s', hashed='%s')>" %(self.username, self.hashed))
-
-# create a class which will be connected to the Books table in the database
-class Book(Base):
-	__tablename__ = "Books"
-	
-	id = Column(Integer, Sequence("Books_id_seq"), primary_key = True)
-	isbn = Column(String)
-	title = Column(String)
-	author = Column(String)
-	year = Column(Integer)
-
-	def __repr__(self):
-		return ("<Book(isbn = '%s', title = '%s', author = '%s', year = '%i')>" 
-		%(self.isbn, self.title, self.author, self.year))
-'''
-
-
-
-
 
 @app.route("/")
 @login_required
 def index():
 	'''Show main page to the user'''	
+	
 	return render_template("index.html")
 
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
 	'''Register new user'''
+	
 	# If the user reaches the route via the GET method render the register form for them.
 	if request.method == "GET":
 		return render_template("register.html")
+	
 	# If the user reaches the route via the POST method process their input
 	else:
+		
 		# prepare the variables
 		username = request.form.get("reg_username")
 		hashed = generate_password_hash(request.form.get("reg_password"))		
 		
 		# username already in the database
-		#if db.query(User).filter_by(username = username).first() != None:
 		s = text("SELECT * FROM users WHERE username = :username")
 		result = db.execute(s, {'username': username}).fetchall()
 		if len(result) != 0:
 			return jsonify({"error": "1"})
+		
 		# success
 		else:
+			
 			# add the new user's username and hashed password to the users table
-			'''
-			new_user = User(username = username, hashed = hashed)
-			db.add(new_user)
-			db.commit()
-
-			'''
 			i = text("INSERT INTO users(username, hashed) VALUES(:username, :hashed)")
 			db.execute(i,{'username': username, 'hashed': hashed})
 		db.commit()
+		
 		# redirect the user to the login page
 		return redirect("/login")
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
 	'''Log user in'''
+
 	# Forget any user_id
 	session.clear()
+	
 	# If the user reaches the route via the GET method, render the login form for them
 	if request.method == "GET":
 		return render_template("login.html")
+	
 	# If the user reaches the route via the POST method, process their input
 	else:
+		
 		# prepare variables to use
 		login_user = request.form.get("login_user")
 		login_pw = request.form.get("login_pw")
+		
 		# Query the database for the user
-		#my_user = db.query(User).filter_by(username = login_user).first()
 		s = text("SELECT * FROM users WHERE username = :login_user")
 		result = db.execute(s, {'login_user': login_user}).fetchone()
+		
 		# Username is not in the database
-		# if my_user == None:
 		if result == None:
 			return jsonify({"error": "1"})
+		
 		# Invalid password
-		# elif not check_password_hash(my_user.hashed, login_pw)
 		elif not check_password_hash(result['hashed'], login_pw):			
 			return jsonify({"error": "2"})
+		
 		# Success
 		else:
-			# Log user in by adding their ID to the session
-			# session["user_id"] = my_user.id			
+			
+			# Log user in by adding their ID to the session	
 			session["user_id"] = result['id']
 		db.commit()
+		
 		# Redirect user to the main page
 		return redirect("/")
 
@@ -176,28 +149,20 @@ def logout():
 # @login_required
 def search():
 	'''Search for books that match the query'''
+
 	# put the user's search terms into a variable
 	q = request.args.get("q")
+
 	# query the database using the search term
 	s = text("SELECT * FROM books WHERE isbn LIKE :q OR title LIKE :q or author LIKE :q LIMIT 10")
 	my_books = db.execute(s, {'q': ('%' + q + '%')}).fetchall()
+	
 	# format results as a dictionary (so that jsonify does not choke)
 	my_list = []
 	for row in my_books:
 		my_list.append({'isbn': row[1], 'title': row[2], 'author': row[3]})
-	'''
-	ORM version (does not work)
-	 q = q.replace("%20", " ")
-	 print(q)
-	 likey = db.query(Book).filter(or_(Book.isbn.match(q), Book.title.match(q), Book.author.match(q))).all()[:10]
-	j = len(likey)
-	books = []
-
-	for i in range(j):
-		books.append({'isbn': likey[i].isbn, 'title': likey[i].title, 'author': likey[i].author})
-	print(books)
-	'''
 	db.commit()
+	
 	# return query results in a json format
 	return jsonify(my_list)
 
@@ -209,42 +174,53 @@ def book(isbn):
 	
 	# the user reached the route via the GET method
 	if request.method == "GET":
-		# my_book = db.query(Book).filter_by(isbn = isbn).first()
+		
 		# find the book the user has selected
 		s = text("SELECT * FROM books WHERE isbn = :isbn")
 		my_book = db.execute(s, {'isbn': isbn}).fetchone()
+		
 		# find all ratings and reviews for the selected book
 		t = text("SELECT username, message, rating FROM comments WHERE book_isbn = :isbn")
 		my_comments = db.execute(t, {'isbn': isbn}).fetchall()
+		
 		# get the Goodreads ratings data through the API
 		goodreads = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "4NAeoxfZQc0UXGetcSEzEA", "isbns": isbn})
 		ratings_count = goodreads.json()["books"][0]["work_ratings_count"]
 		average_rating = goodreads.json()["books"][0]["average_rating"]
 		db.commit()
+		
 		# return the information, ratings and reviews of the selected book
 		return render_template("book.html", isbn = my_book['isbn'], author = my_book['author'],
 		 title = my_book['title'], year = my_book['year'], my_comments = my_comments, j = len(my_comments),
 		 ratings_count = ratings_count, average_rating = average_rating)
+	
 	# the user reached the route via the POST method
 	else:
+		
 		# get the user's comment
 		user_comment = request.form.get("user_comment")
 		rating = request.form.get("rating")
+		
 		# get the user's username
 		a = text("SELECT username FROM users WHERE id = :iden")
 		my_user = db.execute(a, {'iden': session["user_id"]}).fetchone()
+		
 		# find out if the user left a comment on the selected book already
 		b = text("SELECT * FROM comments WHERE username = :my_user AND book_isbn = :isbn")
 		result = db.execute(b, {'my_user': my_user['username'], 'isbn': isbn}).fetchall()
+		
 		# the user did leave a comment already
 		if len(result) != 0:
 			return jsonify({"error": "1"})
+		
 		# the user did not leave a comment yet
 		else:
+			
 			# add the comment to the comments table
 			ins = text("INSERT INTO comments(book_isbn, username, message, rating) VALUES(:isbn, :username, :message, :rating)")
 			db.execute(ins, {'isbn': isbn, 'username': my_user['username'], 'message': str(user_comment), 'rating': rating})
 		db.commit()
+		
 		# refresh the page for the user
 		return redirect("/book/"+isbn)
 
@@ -252,23 +228,30 @@ def book(isbn):
 @login_required
 def api(isbn):
 	'''Return a JSON object with relevant info on the selected book'''
+	
 	# find out if the book is in the database
 	s = text("SELECT * FROM books WHERE isbn = :isbn")
 	my_book = db.execute(s, {'isbn': isbn}).fetchone()
 	db.commit()
+	
 	# book is not found in the database
 	if my_book == None:
+		
 		# return a 404 error
 		abort(404)
+	
 	# book is found in the database
 	else:
+		
 		#grab the relevant info from Goodreads API
 		goodreads = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "4NAeoxfZQc0UXGetcSEzEA", "isbns": isbn})
 		review_count = goodreads.json()["books"][0]["work_reviews_count"]
 		average_rating = goodreads.json()["books"][0]["average_rating"]
+		
 		# arrange the data in a dictionary
 		details = {'title': my_book["title"], 'author': my_book["author"], 'year': my_book["year"],
 		'isbn': my_book["isbn"], 'review_count': review_count, 'average_score': average_rating}
+		
 		# return the data in a JSON format
 		return jsonify(details)
 		
--- import.py
+++ import.py
@@ -14,7 +14,7 @@ conn = engine.connect()
 # create a catalog of Tables
 metadata = MetaData()
 
-# create the Books table
+# create the books table
 books = Table("books", metadata,
 	Column("id", Integer, Sequence("Books_id_seq"), primary_key = True),
 	Column("isbn", String),
@@ -25,43 +25,23 @@ books = Table("books", metadata,
 # create (scoped) session
 db = scoped_session(sessionmaker(bind=engine))
 
-'''
-ORM version
-# declare a mapping
-Base = declarative_base()
-
-# create a class which will be mapped to the database
-class Book(Base):
-	__tablename__ = "Books"
-	
-	id = Column(Integer, Sequence("Books_id_seq"), primary_key = True)
-	isbn = Column(String)
-	title = Column(String)
-	author = Column(String)
-	year = Column(Integer)
-
-	def __repr__(self):
-		return ("<Book(isbn = '%s', title = '%s', author = '%s', year = '%i')>" 
-		%(self.isbn, self.title, self.author, self.year))
-'''
 
 # prepare the SQL statement to be used
 s = text("INSERT INTO books(isbn, title, author, year) VALUES(:a, :b, :c, :d)")
+
 #open books.csv and read it
 with open("books.csv") as csv_file:
 	csv_reader = csv.reader(csv_file, delimiter = ",")
 	line_count = 0
+	
 	# iterate through each entry
 	for row in csv_reader:
+		
 		# skip the headers
 		if line_count == 0:
 			line_count += 1
-		# add each entry to the Books table in the database
+		
+		# add each entry to the books table in the database
 		else:
 			conn.execute(s, a = row[0], b = row[1], c = row[2], d = row[3])
-			# new_book = Book(isbn = row[0], title = row[1], author = row[2], year = int(row[3]))
-			# db.add(new_book)
-
 			line_count += 1
-# commit the changes in the database
-# db.commit()
